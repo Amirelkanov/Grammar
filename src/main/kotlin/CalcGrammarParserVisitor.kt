10176dda71a550ccd26b868333ffdd39cce72302
AmEl <amirelkanov0504@gmail.com>
2022-11-17 16:11:06 +0300

    Optimized imports

--- src/main/kotlin/CalcGrammarParserVisitor.kt
+++ src/main/kotlin/CalcGrammarParserVisitor.kt
@@ -1,18 +1,16 @@
 import antlrGeneratedSource.CalcGrammarBaseVisitor
-import antlrGeneratedSource.CalcGrammarParser
-import antlrGeneratedSource.CalcGrammarParser.IntContext
-import antlrGeneratedSource.CalcGrammarParser.ParenthesesContext
+import antlrGeneratedSource.CalcGrammarParser.*
 
 
 class CalcGrammarParserVisitor : CalcGrammarBaseVisitor<Int?>() {
     private val memory: MutableMap<String, Int> = mutableMapOf()
 
-    override fun visitStatement(ctx: CalcGrammarParser.StatementContext) =
+    override fun visitStatement(ctx: StatementContext) =
         if (ctx.assignment() != null) visitAssignment(ctx.assignment())
         else if (ctx.expression() != null) visit(ctx.expression())
         else null
 
-    override fun visitAssignment(ctx: CalcGrammarParser.AssignmentContext): Int? {
+    override fun visitAssignment(ctx: AssignmentContext): Int? {
         val id = ctx.ID()?.text
         val value = visit(ctx.expression())
         if (id != null && value != null)
@@ -20,17 +18,17 @@ class CalcGrammarParserVisitor : CalcGrammarBaseVisitor<Int?>() {
         return value
     }
 
-    override fun visitSum(ctx: CalcGrammarParser.SumContext) =
+    override fun visitSum(ctx: SumContext) =
         visit(ctx.expression(1))?.let { visit(ctx.expression(0))?.plus(it) }
 
-    override fun visitProduct(ctx: CalcGrammarParser.ProductContext) =
+    override fun visitProduct(ctx: ProductContext) =
         visit(ctx.expression(1))?.let { visit(ctx.expression(0))?.times(it) }
 
     override fun visitInt(ctx: IntContext) = ctx.INT()?.text?.toInt()
 
     override fun visitParentheses(ctx: ParenthesesContext) = visit(ctx.expression())
 
-    override fun visitId(ctx: CalcGrammarParser.IdContext): Int? {
+    override fun visitId(ctx: IdContext): Int? {
         val id = ctx.ID()?.text
         return if (memory.containsKey(id)) memory[id] else 0
     }
